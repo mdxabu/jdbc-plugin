c8c08c6e01b6bd25cf1c358e2554229032c9e8bf
M. <abdullahfakrudeen2020@gmail.com>
2024-04-01 20:17:47 +0530

    added MySQL Dependency
    Signed-off-by: M. <abdullahfakrudeen2020@gmail.com>

--- src/main/java/org/mdxabu/Databases/Main.java
+++
@@ -1,7 +0,0 @@
-package org.mdxabu.Databases;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("jdbc-plugin!");
-    }
-}
---
+++ src/main/java/org/mdxabu/Databases/MySQL.java
@@ -0,0 +1,7 @@
+package org.mdxabu.Databases;
+
+public class MySQL {
+
+
+
+}
