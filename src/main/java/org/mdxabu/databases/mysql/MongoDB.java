bf4740903c00cb413b2d5267507e327dd79a722f
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-06-14 22:40:22 +0530

    initiated mongodb class
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- src/main/java/org/mdxabu/databases/mysql/MongoDB.java
+++
@@ -1,4 +0,0 @@
-package org.mdxabu.databases.mysql;
-
-public class MongoDB {
-}
---
+++ src/main/java/org/mdxabu/databases/nosql/MongoDB.java
@@ -0,0 +1,10 @@
+package org.mdxabu.databases.nosql;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+public class MongoDB {
+    
+    M
+    MongoClient mongoclient = MongoClients.create()
+ }
