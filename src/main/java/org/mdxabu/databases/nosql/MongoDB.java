e1bb9b3bf8661e74e9881a0b55fc28495db08b7b
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-06-16 17:38:30 +0530

    Added getters and setters in mongodb
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- src/main/java/org/mdxabu/databases/nosql/MongoDB.java
+++ src/main/java/org/mdxabu/databases/nosql/MongoDB.java
@@ -1,10 +1,34 @@
 package org.mdxabu.databases.nosql;
 
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoDB {
-    
-    M
-    MongoClient mongoclient = MongoClients.create()
- }
+
+    String ConnectionString;
+    MongoClient mongoClient;
+    MongoDatabase db;
+
+    public void setConnectionString(String connectionstring) {
+        this.ConnectionString = connectionstring;
+    }
+    public String getConnectionString() {
+        return this.ConnectionString;
+    }
+
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    public void setMongoClient(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+    }
+
+    public MongoDatabase getDb() {
+        return db;
+    }
+
+    public void setDb(MongoDatabase db) {
+        this.db = db;
+    }
+}
