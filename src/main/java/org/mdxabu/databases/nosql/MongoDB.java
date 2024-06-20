1f1f37b7f967239012eeb7bbddd703f067edc320
Mohamed Abdullah <abdullahfakrudeen2020@gmail.com>
2024-06-20 11:44:19 +0530

    cleared mongodb

--- src/main/java/org/mdxabu/databases/nosql/MongoDB.java
+++ src/main/java/org/mdxabu/databases/nosql/MongoDB.java
@@ -1,35 +1,9 @@
 package org.mdxabu.databases.nosql;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
 
 public class MongoDB {
 
-    String ConnectionString;
-    MongoClient mongoClient;
-    MongoDatabase db;
-
-    public void setConnectionString(String connectionstring) {
-        this.ConnectionString = connectionstring;
-    }
-    public String getConnectionString() {
-        return this.ConnectionString;
-    }
-
-    public MongoClient getMongoClient() {
-        return mongoClient;
-    }
-
-    public void setMongoClient(String connectionstring) {
-        this.mongoClient = MongoClients.create(connectionstring);
-    }
-
-    public void createDatabase(String databaseName) {
-        db = this.mongoClient.getDatabase(databaseName);
-    }
-
-
+// yet to be finished soon.... 
 
 
 }
