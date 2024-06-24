403dbcf38d18177cdfceca106a9a54169dee0c96
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-06-24 16:33:57 +0530

    Created MongoDB class and Created a setConnectionString(), CreateDatabase(), CreateCollection() and some Getters() and Setters()
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,27 @@ This class only for instant testing for all database.
  */
 
 
-import org.mdxabu.databases.mysql.MySQL;
-
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import org.mdxabu.databases.nosql.MongoDB;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        MySQL db = new MySQL();
-        db.setENDPOINT("localhost");
-        db.setPORT("3306");
-        db.setCredentials("root", "password");
-        db.start();
-        db.useDatabase("jdbctesting");
-        Map<String,String> user = new HashMap<>();
-        user.put("username", "   VARCHAR(20)");
-        user.put("password", "VARCHAR(20)");
-        user.put("age", "INT");
-        db.createTable("users",user);
+    public static void main(String[] args) {
 
+        String mongoConnectionString = "mongodb://localhost:27017";
+
+        MongoDB mongoDB = new MongoDB();
+
+        mongoDB.setMongoConnectionString(mongoConnectionString);
+
+        String databaseName = "jdbctestingmongodb";
+
+        mongoDB.createMongoDatabase(databaseName);
+        System.out.println("Database: " + mongoDB.getMongoDatabase());
+
+        String collectionName = "jdbctestingmongodbcollection";
+        mongoDB.createMongoCollection(collectionName);
+
+
+        System.out.println("MongoDB Connection String: " + mongoDB.getMongoConnectionString());
     }
 
 }
--- src/main/java/org/mdxabu/databases/nosql/MongoDB.java
+++ src/main/java/org/mdxabu/databases/nosql/MongoDB.java
@@ -1,9 +1,67 @@
 package org.mdxabu.databases.nosql;
 
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MongoDB {
 
-// yet to be finished soon.... 
+    String collection_name = null;
+    MongoClient mongoClient;
+    MongoDatabase mongoDatabase;
+    MongoCollection<Document> mongoCollection;
+    private static final Logger logger = LoggerFactory.getLogger(MongoDB.class);
 
-
+    public void setMongoConnectionString(String mongoConnectionString) {
+        try {
+            this.collection_name = mongoConnectionString;
+            this.mongoClient = MongoClients.create(mongoConnectionString);
+            logger.info("MongoDB connection established with connection string: {}", mongoConnectionString);
+        } catch (Exception e) {
+            logger.error("Failed to establish MongoDB connection", e);
+        }
+    }
+    public String getMongoConnectionString() {
+        if (this.mongoClient != null) {
+            return this.collection_name;
+        } else {
+            return "No MongoDB connection established.";
+        }
+    }
+    public void createMongoDatabase(String DatabaseName) {
+        try {
+            if (this.mongoClient != null && DatabaseName != null && !DatabaseName.isEmpty()) {
+                this.mongoDatabase = this.mongoClient.getDatabase(DatabaseName);
+                logger.info("Database '{}' created or accessed successfully", DatabaseName);
+            } else {
+                logger.error("Database name is null or empty, or MongoClient is not initialized.");
+            }
+        } catch (Exception e) {
+            logger.error("Error while creating or accessing database {}", DatabaseName, e);
+        }
+    }
+    public String getMongoDatabase() {
+        if (this.mongoDatabase != null) {
+            return this.mongoDatabase.getName();
+        } else {
+            return "No MongoDB database selected.";
+        }
+    }
+    public void createMongoCollection(String CollectionName) {
+        try {
+            if (this.mongoDatabase != null && CollectionName != null && !CollectionName.isEmpty()) {
+                this.mongoDatabase.createCollection(CollectionName);
+                logger.info("Collection '{}' created successfully", CollectionName);
+            } else {
+                logger.error("Collection name is null or empty, So, We can't create a this collection");
+            }
+        } catch (Exception e) {
+            logger.error("Error while creating collection {}", CollectionName);
+        }
+    }
 }
