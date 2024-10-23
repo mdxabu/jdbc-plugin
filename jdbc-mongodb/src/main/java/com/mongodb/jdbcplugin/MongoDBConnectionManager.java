db69d05a5834259bef559bb0eed1f2820ca5bbd1
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-10-23 22:08:42 +0530

    Added some developer informations and build workflows for different operatng system
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

---
+++ jdbc-mongodb/src/main/java/com/mongodb/jdbcplugin/MongoDBConnectionManager.java
@@ -0,0 +1,10 @@
+package com.mongodb.jdbcplugin;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDBConnectionManager {
+
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
+}
---
+++ jdbc-mongodb/src/main/java/com/mongodb/jdbcplugin/MongoDBQueryExecutor.java
@@ -0,0 +1,4 @@
+package com.mongodb.jdbcplugin;
+
+public class MongoDBQueryExecutor {
+}
